fc8323528de7d102e6a5095a7cbc591d023f5b3d
范端端 <1208674494@qq.com>
2019-03-10 18:50:32 +0800

    .

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-console.log(require('mobx-react'));
+import {inject,observer} from 'mobx-react';
 
+@inject((stores) => ({
+  a: stores.a,
+  b: stores.b
+}))
+@observer
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+
+              Learn React
+            </a>
+          </header>
+        </div>
     );
   }
 }
--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'mobx-react';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider stores={{a:1,b:2}}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
